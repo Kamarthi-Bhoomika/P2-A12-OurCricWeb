69a23eaf55c75563fd7c164e1bdea163fbbe2474
Kamarthi-Bhoomika <kbhoomika05@gmail.com>
2023-08-29 15:36:07 +0530

    MVC Partial views

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using Assignment12.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment12.Controllers
+{
+    public class PlayerController : Controller
+    {
+        List<Team> listTeam = new List<Team>()
+        {
+            new Team(){TeamId = 1,TeamName="India",NOWC=2},
+            new Team(){TeamId = 2,TeamName="Australia",NOWC=4},
+            new Team(){TeamId = 3,TeamName="West Indies",NOWC=2},
+            new Team(){TeamId = 4,TeamName="England",NOWC=1}
+        };
+        // GET: Player
+        public ActionResult Index()
+        {
+            return View(listTeam);
+        }
+    }
+}
